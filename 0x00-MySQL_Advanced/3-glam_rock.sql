acc29988f72f73b89124dac0fdcbe0d04d84c13a
“teepatra” <fagboretomilola@gmail.com>
2024-06-11 17:40:14 +0000

    script that lists all bands with Glam rock as their main style, ranked by their longevity

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- Lets find out how how long the bands were together
+-- up until 2022
+
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+    FROM metal_bands
+    WHERE style LIKE '%Glam rock%'
+    ORDER BY lifespan DESC;
